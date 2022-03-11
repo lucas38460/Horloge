d4316ce90cdf5ee25c3c0734f744a68317f96f71
Lux <92374048+lucas38460@users.noreply.github.com>
2022-03-11 14:46:33 +0100

    test d'un switch case

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ let resetBtn = document.getElementById('set0');
 
 
 
-
+// Test convertisseur de milisecondes en secondes.
 function changeColor() {
-  if (!nIntervId) {
-    nIntervId = setInterval(flashText, 1000);
-  }
+    if (!nIntervId) {
+        nIntervId = setInterval(flashText, 1000);
+    }
 }
 console.log(nIntervId);
 
@@ -19,7 +19,7 @@ console.log(nIntervId);
 // Boutton permettant de remettre le décompteur à 20 minutes
 function reset(x) {
 
-        // Nombre en miliseconde correspondant à 20 minutes.
+    // Nombre en miliseconde correspondant à 20 minutes.
     counter = 1200000;
 }
 
@@ -30,7 +30,7 @@ for (let i = 1; i < 1000; i++) {
     counter++;
     console.log(counter);
     min = Math.floor((counter / 1000 / 60) << 0),
-    sec = Math.floor((counter / 1000) % 60);
+        sec = Math.floor((counter / 1000) % 60);
     console.log(min + ':' + sec);
 }
 
@@ -43,3 +43,58 @@ for (let i = 1; i < 1000; i++) {
 // min = Math.floor((counter / 1000 / 60) << 0),
 // sec = Math.floor((counter / 1000) % 60);
 // console.log(min + ':' + sec);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let number = 0;
+
+for (let i = 0; i < 10000; i++) {
+
+    switch (number) {
+        case number === "0":
+
+            break;
+
+        case number === "1":
+
+            break;
+        case number === "2":
+
+            break;
+        case number === "3":
+
+            break;
+        case number === "4":
+
+            break;
+        case number === "5":
+
+            break;
+        case number === "6":
+
+            break;
+        case number === "7":
+
+            break;
+        case number === "8":
+
+            break;
+        case number === "9":
+
+            break;
+        default:
+            console.log(number);
+            break;
+    }
+}
