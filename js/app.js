d1f25ea9b74d4e1f5479f6fc5bb8deb50163a056
Lux <92374048+lucas38460@users.noreply.github.com>
2022-05-10 15:55:50 +0200

    changement sur le css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,60 @@
-let minutesHMTL = document.getElementById('minutes').innerText = minutes;
-let secondesHTML = document.getElementById('secondes').innerText = secondes;
-
-let secondes = 0;
 let minutes = 24;
+let secondes = 0;
 
+const tiles = document.getElementsByClassName('tile');
+for (const tile of tiles) {
+    tile.classList.add('grey');
+}
 
-buttonStart => {
-    let decompte = setTimeout("buttonStart()", 1000);
-    seconde -= 1;
+const huits = document.getElementsByClassName('huit');
 
-    if (minutes == 0)  {
-        minutes--;
+function getHuitTiles(position){
+    return huits[position].children;
+}
+
+counter = setInterval(decrement, 100);
+
+function decrement(){
+    if(minutes > 0 || secondes > 0){
+        if(secondes > 0){
+            secondes --;
+        } else {
+            secondes = 59;
+            minutes --;
+        }
+        const time = convertCounterToString(minutes, secondes);
+        render(time);
+    } else {
+        clearInterval(counter);
     }
-    
-    if (secondes == 0) {
-        secondes = 59;
+}
+
+function convertCounterToString(minutes, secondes){
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    } else {
+        minutes = "" + minutes;
     }
-    
+    if(secondes < 10){
+        secondes = "0" + secondes;
+    } else {
+        secondes = "" + secondes;
+    }
+    return minutes+ secondes;
+}
+
+
+function render(time){
+    for (let index = 0; index < time.length; index++) {
+        const number = time[index];
+        const tiles = getHuitTiles(index);
+        for (const tile of tiles) {
+            if(tile.classList[1].includes(number)){
+                tile.classList.add('red');
+            } else {
+                tile.classList.remove('red');
+            }
+        }
+
+    }
 }
