e37bc86b14da86c5531d2103d3b2647a31ed66c4
Lux <92374048+lucas38460@users.noreply.github.com>
2022-05-11 16:35:58 +0200

    changement des classes du code js

--- js/app.js
+++ js/app.js
@@ -1,60 +1,49 @@
 let minutes = 24;
 let secondes = 0;
 
-const tiles = document.getElementsByClassName('tile');
-for (const tile of tiles) {
-    tile.classList.add('grey');
+const traitS = document.getElementsByClassName("trait");
+for (const traitS of tiles) {
+  traitS.classList.add("grey");
 }
 
-const huits = document.getElementsByClassName('huit');
+timer = setInterval(decrement, 100);
 
-function getHuitTiles(position){
-    return huits[position].children;
-}
-
-counter = setInterval(decrement, 100);
-
-function decrement(){
-    if(minutes > 0 || secondes > 0){
-        if(secondes > 0){
-            secondes --;
-        } else {
-            secondes = 59;
-            minutes --;
-        }
-        const time = convertCounterToString(minutes, secondes);
-        render(time);
+function decrement() {
+  if (minutes > 0 || secondes > 0) {
+    if (secondes > 0) {
+      secondes--;
     } else {
-        clearInterval(counter);
+      secondes = 59;
+      minutes--;
     }
+  } else {
+    clearInterval(counter);
+  }
 }
 
-function convertCounterToString(minutes, secondes){
-    if(minutes < 10){
-        minutes = "0" + minutes;
-    } else {
-        minutes = "" + minutes;
-    }
-    if(secondes < 10){
-        secondes = "0" + secondes;
-    } else {
-        secondes = "" + secondes;
-    }
-    return minutes+ secondes;
+function CounterAString(minutes, secondes) {
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  } else {
+    minutes = "" + minutes;
+  }
+  if (secondes < 10) {
+    secondes = "0" + secondes;
+  } else {
+    secondes = "" + secondes;
+  }
+  return minutes + secondes;
 }
 
-
-function render(time){
-    for (let index = 0; index < time.length; index++) {
-        const number = time[index];
-        const tiles = getHuitTiles(index);
-        for (const tile of tiles) {
-            if(tile.classList[1].includes(number)){
-                tile.classList.add('red');
-            } else {
-                tile.classList.remove('red');
-            }
-        }
-
+function rendu(time) {
+  for (let index = 0; index < time.length; index++) {
+    const number = time[index];
+    for (const traitS of traitS) {
+      if (traitS.classList[1].includes(number)) {
+        traitS.classList.add("red");
+      } else {
+        traitS.classList.remove("red");
+      }
     }
+  }
 }
