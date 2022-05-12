94246908961771089702a5bbccdc90931c31e25e
Lux <92374048+lucas38460@users.noreply.github.com>
2022-05-12 16:00:54 +0200

    add function minuteur

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 let minutes = 24;
 let secondes = 0;
 
-const trait = document.querySelectorAll('trait');
+const trait = document.getElementsByClassName('trait');
 for (let i = 0; i < trait.length; i++) {
   trait.classList.add("grey");
 }
+
+function counter(e) {
+  if (secondes <= 0) {
+    secondes = 59;
+  } else if (secondes > 0) {
+    secondes --;
+  } else {
+    clearInterval(counter);
+  }
+}
+
+setInterval(counter, 1000);
