81eb29314219c03872a9e72d849f946ddd7df670
Lux <92374048+lucas38460@users.noreply.github.com>
2022-05-12 16:42:56 +0200

    changement de façon de faire avec le html

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 let minutes = 24;
 let secondes = 0;
 
-const trait = document.getElementsByClassName('trait');
+const trait = document.getElementsByClassName("trait");
 for (let i = 0; i < trait.length; i++) {
   trait.classList.add("grey");
 }
 
 function counter(e) {
-  if (secondes <= 0) {
+  if (secondes <= 0 && minutes > 0) {
     secondes = 59;
+    minutes --;
   } else if (secondes > 0) {
-    secondes --;
+    secondes--;
   } else {
     clearInterval(counter);
   }
+  console.log(minutes);
+  console.log(secondes);
 }
 
 setInterval(counter, 1000);
+
+function name(params) {
+  
+}
+
+// function convertisseur(tempsSec, tempsMin) {}
